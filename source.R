8077972f46b092dbc162d239367cb28b1815ddc0
purudpd <purusharth96@gmail.com>
2019-12-27 19:00:42 +0100

    Added code for RMarkdown. Added PDF generated.

--- source.R
+++ source.R
@@ -1,3 +1,4 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
 print("Fixed SSH based authentication: https://applyr.blogspot.com/2014/05/rstudio-pushing-to-github-with-ssh.html")
+print("This is good")
