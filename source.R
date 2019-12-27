d11c40415a5cc66eb4e0d94c5e6698712279ec64
purudpd <purusharth96@gmail.com>
2019-12-27 02:53:07 +0100

    Fixed SSH key based authentication. Added reference website.

--- source.R
+++ source.R
@@ -1,2 +1,3 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
+print("Fixed SSH based authentication: https://applyr.blogspot.com/2014/05/rstudio-pushing-to-github-with-ssh.html")
